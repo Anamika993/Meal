109d3b4cc71b04ba6d750f15534009aefe87e2a6
anamikakrivarshney@gmail.com <anamikakrivarshney@gmail.com>
2024-04-04 18:40:30 +0530

    first commit

--- screens/ContactUs.js
+++ screens/ContactUs.js
@@ -2,106 +2,25 @@ import { useState } from 'react';
 import {Text,View, TextInput, StyleSheet, Pressable} from 'react-native';
 import SignUp from './SignUpScreen';
 
-function ContactUs({navigation}){
+function ContactUs(){
 
-return (
-<View style={styles.appContainer}>
-    <View style={styles.textArea}>
-        <Text style={styles.text}>ContactUs</Text>
-    </View>
-    <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Enter Email'/>
-        <TextInput style={styles.textInput} placeholder='Enter Password'/>
-        <Pressable style={styles.button} onPress={()=>navigation.navigate('Categories')}>
-            <Text style={styles.texts}>Login</Text>
-        </Pressable>
-    </View>
-    <View style={styles.check}>
-        <Text style={styles.setText}>
-            Don't have an account?
+  return (
+    <View>
+        <Text style={styles.text}>
+            ContactUs
         </Text>
-        <Pressable onPress={()=>navigation.navigate('SignUp')}>
-            <Text style={styles.setLoginButton}>SignUp</Text>
-        </Pressable>
     </View>
-    
-</View>
 )
 }
 
 export default ContactUs;
 
 const styles = StyleSheet.create({
-    appContainer:{
-    //   flex:1,
-      paddingTop:50,
-      paddingHorizontal:16,
-      backgroundGradient: "vertical",
-      backgroundGradientTop: "#603813",
-      backgroundGradientBottom: " #b29f94 ",
-      height:'100%'
-
-    },
-    textArea:{
-        // borderWidth:1,
-        // borderColor:'white',
-        // justifyContent:"center",
-        // alignItems:'center',
-        marginTop:100,
-        marginBottom:15,
-    },
-    text:{
-        textAlign:'center',
-        fontSize:35,
-        fontWeight:'bold',
-        color:'black',
-    },
-    inputContainer:{
-    //   flex:1,
-      flexDirection:'column',
-      justifyContent:'space-between',
-      borderBottomColor: 'white',
-      borderColor:"white",
-      alignItems:'center',
-      marginBottom:24,
-    //   color:'white's
-    },
-    textInput:{
-        marginBottom:12,
-      borderWidth:1,
-      color:'black',
-      width:'80%',
-      marginRight:5,
-      padding:5
-    },
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: 'black',
-      },
-      texts: {
-        fontSize: 16,
-        lineHeight: 21,
-        fontWeight: 'bold',
-        letterSpacing: 0.25,
-        color: 'white',
-      },
-      check:{
-        marginTop:-20,
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center'
-      },
-      setText:{
-        color:"white",
-        marginRight:5
-      },
-      setLoginButton:{
-        color:"blue"
-      }
+  text:{
+    fontWeight:'bold',
+    fontSize:40,
+    textAlign:'center',
+    color:'white'
+}
 }
 )
